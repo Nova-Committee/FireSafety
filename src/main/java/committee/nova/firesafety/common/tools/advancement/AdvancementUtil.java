2549f4e3eecbe4e4b4cf1845a8747272a581fa8a
MikhailTapio <mikhailtapio13468@163.com>
2022-05-24 11:46:05 +0800

    Format

--- src/main/java/committee/nova/firesafety/common/tools/advancement/AdvancementUtil.java
+++ src/main/java/committee/nova/firesafety/common/tools/advancement/AdvancementUtil.java
@@ -7,23 +7,18 @@ import net.minecraft.server.level.ServerPlayer;
 
 public class AdvancementUtil {
     public static void tryAwardAdvancement(ServerPlayer player, ResourceLocation advancementId) {
-        if (player == null) {
-            return;
-        }
+        if (player == null) return;
         final var advancement = player.server.getAdvancements().getAdvancement(advancementId);
-        if (advancement == null) {
-            return;
-        }
+        if (advancement == null) return;
         final var progress = player.getAdvancements().getOrStartProgress(advancement);
         if (!progress.isDone()) completeTheAdvancement(player, advancement, progress);
     }
 
     private static void completeTheAdvancement(ServerPlayer player, Advancement advancement, AdvancementProgress progress) {
         final var iterator = progress.getRemainingCriteria().iterator();
-        if (iterator.hasNext()) {
-            player.getAdvancements().award(advancement, iterator.next());
-            completeTheAdvancement(player, advancement, progress);
-        }
+        if (!iterator.hasNext()) return;
+        player.getAdvancements().award(advancement, iterator.next());
+        completeTheAdvancement(player, advancement, progress);
     }
 
 }
